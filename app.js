0d7761205d33f158733e55993801c01cb8d1a344
Priyankshu-07 <priyankshusinha98@gmail.com>
2025-06-22 19:01:06 +0530

    added process.env.PORT

--- app.js
+++ app.js
@@ -80,6 +80,10 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 
 // Start server
-server.listen(3000, () => {
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
+
+
